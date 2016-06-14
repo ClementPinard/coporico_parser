a8f6af5aef387790e057d7a5c18aabc7f1b6802e
Clément Pinard <clempinard@gmail.com>
2016-06-15 01:53:52 +0200

    add pandas table and matplotlib rendering of histograms

--- Parser.py
+++ Parser.py
@@ -4,9 +4,16 @@ Hopefully, more to come you're welcomed to contribute !
 usage : python --company <string> --cookie <string>
 '''
 
+
+
+from __future__ import unicode_literals
 import requests
 import bs4 as BeautifulSoup
+import pandas as pd #LOL
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+nullfmt = NullFormatter()         # no labels
 import argparse
 
 parser = argparse.ArgumentParser(description='A simple script to make some nice statistics with your corporico team')
@@ -24,75 +31,136 @@ matches = []
 teams= dict()
 scores= dict()
 
-k=1
-while True:
-    j=1
-    pronos_match = []
-    while True:
-        payload = {'page': j}
-        r=requests.get('http://'+args.company+'.corporico.fr/matches/' + str(k),cookies=cookies, params=payload)
-        print r.url
-        if r.url == 'http://' + args.company + '.corporico.fr/sessions/new':
-            print("cookie or company error")
-            exit()
-             
-        soup = BeautifulSoup.BeautifulSoup(r.text, "lxml")
-        '''
-        <div class="leaderboard-item-value">
-                    2 - 0
-                  </div>
-        '''
-        #print soup.div
-        i=0
-        if j==1:
-            countries = soup.findAll('div', attrs={'class': 'match-item-team-name'})
-            countries = [country.text.strip() for country in countries]
-            final_score = soup.findAll('div',attrs={'class': 'has-score'})
-            final_score = [int(score.text.strip()) for score in final_score]
+def get_match_pronos(match_ids):
+    names = []
+    teams = []
+    fscoresAlpha = []
+    fscoresBeta = []
+    pronosAlpha = []
+    pronosBeta = []
+    countriesAlpha = []
+    countriesBeta = []
+    match_IDs = []
+    for k in match_ids:
+        j=1
+        while True:
+            payload = {'page': j}
+            r=requests.get('http://'+args.company+'.corporico.fr/matches/' + str(k),cookies=cookies, params=payload)
+            print(r.url)
+            if r.url == 'http://' + args.company + '.corporico.fr/sessions/new':
+                print("cookie or company error")
+                exit()
+                 
+            soup = BeautifulSoup.BeautifulSoup(r.text, "lxml")
+            
+            if j==1:
+                countries = soup.findAll('div', attrs={'class': 'match-item-team-name'})
+                countries = [country.text.strip() for country in countries]
+                final_score = soup.findAll('div',attrs={'class': 'has-score'})
+                final_score = [int(score.text.strip()) for score in final_score]
+            
+            
+            entries = soup.findAll('div', attrs={'class': 'leaderboard-item'})
+            if(len(entries) == 0):
+                break
+            for soup in entries :
+                #print soup
+                nameAndTeam = soup.find('div', attrs={'class': 'leaderboard-item-cell-team'}).find_next('div', attrs={'class': 'leaderboard-item-label'}).text.strip()
+                name = nameAndTeam[:nameAndTeam.find('\n')]
+                if 'TEAM' in nameAndTeam:
+                    team = nameAndTeam[nameAndTeam.find('TEAM ') + 5:]
+                else:
+                    team = 'None'
+                prono = soup.find('div', attrs={'class': 'leaderboard-item-cell-bet-count'}).find_next(True).find_next(True).text
+                prono = prono.split('-')
+                prono = [int(prono[0]),int(prono[1])]
+                pronosAlpha.append(prono[0])
+                pronosBeta.append(prono[1])
+                teams.append(team)
+                names.append(name)
+                fscoresAlpha.append(final_score[0])
+                fscoresBeta.append(final_score[1])
+                countriesAlpha.append(countries[0])
+                countriesBeta.append(countries[1])
+                match_IDs.append(k)
+                
+            j = j+1
+    data = {'name':names,
+            'team':teams,
+            'fscoreAlpha':fscoresAlpha,
+            'fscoreBeta':fscoresBeta,
+            'pronoAlpha':pronosAlpha,
+            'pronoBeta':pronosBeta,
+            'countryAlpha':countriesAlpha,
+            'countryBeta':countriesBeta,
+            'matchID':match_IDs
+            }
+    dataset = pd.DataFrame(data)
+    return dataset
+        
+#dataset = get_match_pronos([1,2,3,4,5,6,7,8,39,40,11,12])
+#dataset.to_csv('test.csv',encoding='utf-8', index=False)
+dataset = pd.read_csv('test.csv')
+
+def plot_histograms():
+    for match in dataset['matchID'].unique() :
+        matchdata = dataset[dataset['matchID'] == match]
+        
+        countryAlpha = matchdata['countryAlpha'].iloc[0]
+        countryBeta = matchdata['countryBeta'].iloc[0]
+        
+        scoreAlpha = matchdata['fscoreAlpha'].iloc[0]
+        scoreBeta = matchdata['fscoreBeta'].iloc[0]
         
         
-        entries = soup.findAll('div', attrs={'class': 'leaderboard-item'})
-        for soup in entries :
-            #print soup
-            nameAndTeam = soup.find('div', attrs={'class': 'leaderboard-item-cell-team'}).find_next('div', attrs={'class': 'leaderboard-item-label'}).text.strip()
-            name = nameAndTeam[13:nameAndTeam.find('\n')]
-            team = nameAndTeam[nameAndTeam.find('TEAM ') + 5]
-            #print name
-            #print team
-            prono = soup.find('div', attrs={'class': 'leaderboard-item-cell-bet-count'}).find_next(True).find_next(True).text
-            prono = prono.split('-')
-            prono = [int(prono[0]),int(prono[1])]
-            pronos_match.append(prono)
-            #print (prono)
-            i=i+1
-            #print(i)
-        if i == 0:
-            break
-        j = j+1
-    if j==1:
-        break
-    else:
-        matches.append(k)
-        pronos[k] = pronos_match
-        teams[k] = countries
-        scores[k] = final_score
-    k=k+1
+        prono_x = matchdata['pronoAlpha']
+        prono_y = matchdata['pronoBeta']
+        
+        edges = [0,1,2,3,4,5,6]
+        hist2d = np.histogram2d(prono_x, prono_y,bins = (edges,edges))
+        
+        print(countryAlpha + ' vs ' + countryBeta)
+        print(str(scoreAlpha) + ' - ' + str(scoreBeta))
+        
+        # definitions for the axes
+        left, width = 0.1, 0.62
+        bottom, height = 0.1, 0.62
+        bottom_h = left_h = left + width + 0.02
+        
+        
+        rect_scatter = [left, bottom, width, height]
+        rect_histx = [left, bottom_h, width, 0.2]
+        rect_histy = [left_h, bottom, 0.2, height]
+        # start with a rectangular Figure
+        plt.figure(match,figsize=(5, 5))
+        axScatter = plt.axes(rect_scatter)
+        axHistx = plt.axes(rect_histx)
+        axHisty = plt.axes(rect_histy)
+        
+        # no labels
+        axHistx.xaxis.set_major_formatter(nullfmt)
+        axHisty.yaxis.set_major_formatter(nullfmt)
+        
+        # the scatter plot:
+        axScatter.imshow(np.transpose(hist2d[0]),interpolation = 'nearest')
+        
+        # now determine nice limits by hand:
+        binwidth = 1
+        lim = 5.5
+        
+        axScatter.set_xlim((-0.5, lim))
+        axScatter.set_ylim((-0.5, lim))
+        
+        bins = np.arange(-lim, lim + binwidth, binwidth)
+        axHistx.hist(prono_x, bins=bins)
+        axHisty.hist(prono_y, bins=bins, orientation='horizontal')
+        axHistx.set_xlim(axScatter.get_xlim())
+        axHisty.set_ylim(axScatter.get_ylim())
+        axHisty.set_xticks([125,250])
+        axHistx.set_yticks([125,250])
+        
+        axHistx.set_title(countryAlpha + ' - ' + str(scoreAlpha))
+        axHisty.set_title(countryBeta + '\n' + str(scoreBeta))
+    plt.show()
     
-    
-for match in matches :
-    countries = teams[match]
-    score = scores[match]
-    prono_x = np.array([k[0] for k in pronos[match]])
-    prono_y = np.array([k[1] for k in pronos[match]])
-    
-    edges = [0,1,2,3,4,5,6]
-    
-    histx = np.histogram(prono_x,bins = edges)
-    histy = np.histogram(prono_y,bins = edges)
-    hist2d = np.histogram2d(prono_x, prono_y,bins = (edges,edges))
-    
-    print countries[0] + ' vs ' + countries[1]
-    print str(score[0]) + ' - ' + str(score[1])
-    print histx[0]
-    print histy[0]
-    print hist2d[0]
+plot_histograms()
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # corporico_parser
 A simpe tool to parse all your company's bets and to make more advanced statistics with them
-You need [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/) and [Requests](http://docs.python-requests.org/en/master/) Installed in you python PATH
+You need [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/), [Requests](http://docs.python-requests.org/en/master/) and [Pandas](http://pandas.pydata.org/) Installed in you python PATH
 
-Written in python 2.7
+Written in python 3.5
